3377d7f9485297b66d376eb9e835a147a05c0a4c
Alex Melo <alexmelo84@hotmail.com>
2025-02-24 12:06:58 -0300

    Organizando os padrões em um diretório

--- index.php
+++ index.php
@@ -7,27 +7,27 @@
                 Creational Patterns
                 <ul>
                     <li>
-                        <a href="/creational/abstract-factory" target="_blank">
+                        <a href="/patterns/creational/abstract-factory" target="_blank">
                             Abstract Factory
                         </a>
                     </li>
                     <li>
-                        <a href="/creational/builder" target="_blank">
+                        <a href="/patterns/creational/builder" target="_blank">
                             Builder
                         </a>
                     </li>
                     <li>
-                        <a href="/creational/factory-method" target="_blank">
+                        <a href="/patterns/creational/factory-method" target="_blank">
                             Factory Method
                         </a>
                     </li>
                     <li>
-                        <a href="/creational/prototype" target="_blank">
+                        <a href="/patterns/creational/prototype" target="_blank">
                             Prototype
                         </a>
                     </li>
                     <li>
-                        <a href="/creational/singleton" target="_blank">
+                        <a href="/patterns/creational/singleton" target="_blank">
                             Singleton
                         </a>
                     </li>
